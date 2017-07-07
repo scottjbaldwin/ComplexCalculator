bca12a8601d8df623c2f81499ba285c4b780a14c
Scott Baldwin <scott.baldwin@readify.net>
2017-07-07 13:11:08 +1000

    Added Build Script

---
+++ build.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+
+Builds and runs tests for the Complex Calculator project
+#>
+if (-not (Get-Command MsBuild -ErrorAction SilentlyContinue))
+{
+	Get-VsVars32
+}
+
+MSBuild .\src\ComplexCalculator\ComplexCalculator.sln
+
+.\src\ComplexCalculator\packages\xunit.runner.console.2.3.0-beta3-build3705\tools\net452\xunit.console.exe .\src\ComplexCalculator.Tests\bin\Debug\ComplexCalculator.Tests.dll
