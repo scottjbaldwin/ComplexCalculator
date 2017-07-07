834a4e7ebe9c555cfe1cf1d01f75fca0601611b1
Scott Baldwin <scott.baldwin@readify.net>
2017-07-07 15:00:55 +1000

    Implement ToString

--- src/ComplexCalculator.Tests/ComplexNumberTestClass.cs
+++ src/ComplexCalculator.Tests/ComplexNumberTestClass.cs
@@ -42,5 +42,20 @@ namespace ComplexCalculator.Tests
             // Assert
             Assert.Equal(5, mod);
         }
+
+        [Theory()]
+        [InlineData(3.5, -2.8, "3.5-2.8i")]
+        [InlineData(7.2, 4.1, "7.2+4.1i")]
+        public void WhenToStringCalled_ReturnsStringREpresentationOfComplexNumber(double real, double imaginary, string result)
+        {
+            // Arrange
+            var num = new ComplexNumber { Real = real, Imaginary = imaginary };
+
+            // Act
+            var str = num.ToString();
+
+            // Assert
+            Assert.Equal(result, str);
+        }
     }
 }
